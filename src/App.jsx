1390a622736dded0a0265905bc1c7e517f6c8bf5
joaofaria-src <17826036+joaofaria-src@users.noreply.github.com>
2024-11-25 21:26:56 +0000

    improvements and fully mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-
 import React, { useState } from 'react';
-import ProductList from './ProductList';
 import './App.css';
-import AboutUs from './AboutUs';
+import ProductList from './ProductList';
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -14,32 +11,38 @@ function App() {
 
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
+      {!showProductList && (
+        <div className="landing-page">
+          <div className="background-image"></div>
+          <div className="landing-content">
+            <h1>Welcome to Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
           </div>
+          <div className="about-us">
+            <p>
+              At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a
+              wide range of high-quality plants that enhance the beauty of your surroundings and contribute to a
+              sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every
+              plant enthusiast.
+            </p>
+            <p>
+              Whether you're a seasoned gardener or starting your green journey, we're here to support you every step of
+              the way. Let us help you find the perfect plant for your home or office.
+            </p>
           </div>
-
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
-      </div>
+        </div>
+      )}
+      {showProductList && (
+        <div className="product-list-container">
+          <ProductList />
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
-
-
-
